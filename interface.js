c35f0db5436ec67c745f7a4b09d4a36470f7c442
jhozera <152558072+jhozera@users.noreply.github.com>
2024-10-01 00:09:57 -0300

    Add files via upload

---
+++ interface.js
@@ -0,0 +1,6 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const barra = document.getElementById('barra');
+    
+    barra.classList.toggle('show-menu');
+  });
+  
