74962484ba33860265ab35ce66226460b6b4ce31
jhozera <152558072+jhozera@users.noreply.github.com>
2024-10-01 23:57:36 -0300

    Update interface.js

--- interface.js
+++ interface.js
@@ -1,6 +1,19 @@
 document.getElementById('menu-toggle').addEventListener('click', function() {
-    const barra = document.getElementById('barra');
-    
-    barra.classList.toggle('show-menu');
-  });
-  
+  const barra = document.getElementById('barra');
+  barra.classList.toggle('show-menu');
+});
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.car');
+const totalSlides = slides.length;
+
+function changeSlide(direction) {
+  if (direction === 1 && currentSlide < totalSlides - 1) {
+    currentSlide++;
+  } else if (direction === -1 && currentSlide > 0) {
+    currentSlide--;
+  }
+  const offset = -currentSlide *859 / totalSlides;
+  document.querySelector('.slide-container').style.transform = `translateX(${offset}%)`;
+}
